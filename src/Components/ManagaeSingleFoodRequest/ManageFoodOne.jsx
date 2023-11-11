8ee571a253d4d041ee1f062df9545f854e703b74
Mahmodul7314 <mahmudulhasan7314@gmail.com>
2023-11-11 07:56:52 -0800

    request food delivered status complete

--- src/Components/ManagaeSingleFoodRequest/ManageFoodOne.jsx
+++ src/Components/ManagaeSingleFoodRequest/ManageFoodOne.jsx
@@ -6,8 +6,8 @@
 const ManageFoodOne = ({food}) => {
 const{_id,userEmail,date,foodStatus,userName,userImage}=food;
 
-const handleConfirm = id =>{
-  fetch(`http://localhost:requestFood/${id}`,{
+const handleDelivered = id =>{
+  fetch(`http://localhost:3000/requestFood/${id}`,{
    method:'PATCH',
    headers:{
      'content-type': 'application/json'
@@ -18,7 +18,7 @@ const handleConfirm = id =>{
  .then(data =>{
   console.log(data)
   if(data.modifiedCount > 0){
-    alert('statu change successfull')
+    alert('status change successfull')
   }
  })
   
@@ -29,7 +29,7 @@ const handleConfirm = id =>{
 }
     return (
       <div>
-      {foodStatus ==='Available'?<>
+    
         <div className="bg-amber-50 py-20">
             <div className="px-4 py-8 relative bg-gradient-to-r from-indigo-200 ... shadow-xl flex w-full max-w-[30rem] flex-col rounded-xl bg-transparent bg-clip-border text-gray-700">
   <div className="relative flex items-center gap-4 pt-0 pb-8 mx-0 mt-4 overflow-hidden text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border">
@@ -58,18 +58,15 @@ const handleConfirm = id =>{
   <div className=" text-center mb-6">
     
         { 
-          foodStatus === 'Available'? <button className="font-bold btn btn-warning ">Status Available</button>
-           : <button onClick={()=>handleDelivered(_id)} className="btn btn-success">Delivered</button>
+          foodStatus === 'Delivered'? <button className="font-bold btn btn-warning ">Status Delivered</button>
+           : <button onClick={()=>handleDelivered(_id)} className="btn btn-success">Status Available</button>
       }
     
   </div>
 </div>
         </div>
-        </>
-        :
-        <div className="w-full bg-amber h-[60vh]">
-          <h2 className="text-center text-4xl text-gray-800">This food is No Requested</h2>
-        </div>}
+        
+       
         </div>
     );
 };
